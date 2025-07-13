0a4fc85947524b4a12e814940e529edbff6d22d3
yisus <yisus@JESUSGNZ>
2025-07-13 17:17:18 -0600

    Apis de Articulo

--- Models/ArticuloDto.cs
+++ Models/ArticuloDto.cs
@@ -25,4 +25,14 @@ namespace Sistema_Almacen_MariaDB.Models
         public Nullable<int> ID_Linea { get; set; }
         public Nullable<int> ID_Medida { get; set; }
     }
+
+    public class ArticulosModel
+    {
+        public int ID_Articulo { get; set; }
+        public string Nombre_Articulo { get; set; }
+        public string Descripcion_Articulo { get; set; }
+        public string Numero_Parte { get; set; }
+        public Nullable<int> ID_Linea { get; set; }
+        public Nullable<int> ID_Medida { get; set; }
+    }
 }
